9f52fadd031a47428d43929ee237200453f421e7
telbousadni <taha.elbousadni@gmail.com>
2023-10-22 14:48:14 -0700

    3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,36 +1,34 @@
 #!/usr/bin/python3
-"""Write a script that starts a Flask web application"""
-
+""" module doc """
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def main():
-    """returns text"""
+def hello():
+    """ def doc """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
-def route_hbnb():
-    """text"""
+def hbnb():
+    """ def doc """
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """to print /c"""
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+def c(text):
+    """ def doc """
+    return 'c {}'.format(text.replace("_", " "))
 
 
-@app.route("/python", defaults={'text': "is cool"}, strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python_display(text):
-    """Display “Python ”"""
-    return "".join(["Python ", text.replace("_", " ")])
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """ def doc """
+    return 'Python {}'.format(text.replace("_", " "))
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000))
+    app.run(host="0.0.0.0", port=5000)
