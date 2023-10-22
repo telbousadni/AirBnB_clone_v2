dd3dd09561c68e8d09f80dc3e7c5d1cff46606bf
telbousadni <taha.elbousadni@gmail.com>
2023-10-22 14:59:31 -0700

    fix 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,49 +1,45 @@
 #!/usr/bin/python3
-"""Write a script that starts a Flask web application"""
-
+""" module doc """
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def main():
-    """returns text"""
+def hello():
+    """ def doc """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
-def route_hbnb():
-    """text"""
+def hbnb():
+    """ def doc """
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """to print /c"""
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+def c(text):
+    """ def doc """
+    return 'c {}'.format(text.replace("_", " "))
 
 
-@app.route("/python", defaults={'text': "is cool"}, strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python_display(text):
-    """Display “Python ”"""
-    return "".join(["Python ", text.replace("_", " ")])
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """ def doc """
+    return 'Python {}'.format(text.replace("_", " "))
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def n_is_integer(n):
-    """text"""
-    if type(n) is int:
-        return "{} is a number".format(n)
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ def doc """
+    return '{} is a number'.format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def n_is_integer_html(n):
-    """text"""
-    if type(n) is int:
-        return render_template("5-number.html", number=n)
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    return render_template('5-number.html', number=n)
 
 
 if __name__ == "__main__":
